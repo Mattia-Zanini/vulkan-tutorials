b0db0c00e3641a1994e54e05fdfba1946988aec3
Mattia Zanini <mattzanini2@gmail.com>
2026-07-27 20:11:11 +0200

    lezione 01 - creazione window

--- vulkan-game-engine/lve_window.hpp
+++ vulkan-game-engine/lve_window.hpp
@@ -1,1 +1,32 @@
 #pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <string>
+
+namespace lve {
+
+    class LveWindow {
+
+    public:
+        LveWindow(int w, int h, std::string name);
+        ~LveWindow();
+
+        // in questo modo evito che la finestra possa essere copiata
+        LveWindow(const LveWindow&) = delete;
+        LveWindow& operator=(const LveWindow&) = delete;
+
+        bool shouldClose() { return glfwWindowShouldClose(window); }
+
+    private:
+        void initWindow();
+
+        const int width;
+        const int height;
+
+        std::string windowName;
+        GLFWwindow* window;
+    };
+
+}
